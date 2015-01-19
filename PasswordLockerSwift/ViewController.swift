2231f3c97fa25b015585317c8dfbf3c4aa74a0ab
Eray Diler <eray.diler@gmail.com>
2015-01-19 18:22:04 +0200

    Initial Commit

---
+++ PasswordLockerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordLockerSwift
+//
+//  Created by Eray on 19/01/15.
+//  Copyright (c) 2015 Eray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
