670f6ee0694eaef0bab1fb07db728097c125d057
Eray Diler <eray.diler@gmail.com>
2016-04-30 23:50:54 +0300

    Added new view controller for settings screen.

--- PasswordLockerSwift/Classes/Controller/Settings/SettingsViewController.swift
+++ PasswordLockerSwift/Classes/Controller/Settings/SettingsViewController.swift
@@ -8,19 +8,16 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
